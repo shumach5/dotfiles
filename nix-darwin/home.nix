1feac1b76597d7c8b58a1f5315cd60317f72c0f6
shumach5 <shumach5@gmail.com>
2025-04-01 11:56:43 -0700

    Add oh-my-zsh configuration and additional packages in home.nix

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -4,17 +4,31 @@
   # Specify the macOS username.
   home.username = "shuheieda";
   home.homeDirectory = "/Users/shuheieda";
-
-  # Enable zsh as the user's shell.
-  programs.zsh.enable = true;
-
   # Define user-specific packages to be installed.
   home.packages = with pkgs; [
     bat
     htop
     slack
+    cowsay
+    oh-my-zsh
   ];
+
+  home.file = {
+    #".zshrc".source = config.lib.file.mkOutOfStoreSymlink "/Users/shuheieda/dotfiles/.zshrc";
+  };
+
+  # Enable zsh as the user's shell.
+  programs.zsh.enable = true;
+
+  programs.zsh.oh-my-zsh = {
+    enable = true;
+    plugins = [ ];
+    theme = "agnoster";
+  };
 
   # Set the Home Manager state version. Adjust this value according to your Home Manager version.
   home.stateVersion = "23.11";
+
+  # Create symlink to custom .zshrc
+  # home.file.".zshrc".source = config.lib.file.mkOutOfStoreSymlink "/Users/shuheieda/dotfiles/.zshrc";
 }
